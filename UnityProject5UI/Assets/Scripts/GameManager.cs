37e0f74891a97dddd18dec8dc14f419689b9920c
AndreyGV <grechko.games@gmail.com>
2022-06-27 18:16:07 +0300

    1

--- UnityProject5UI/Assets/Scripts/GameManager.cs
+++ UnityProject5UI/Assets/Scripts/GameManager.cs
@@ -1,15 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
-{
+{ 
     public List<GameObject> targets;
+    public TextMeshProUGUI scoreText;
+
+    private int score;
     private float spawnRate = 1.0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(SpawnTargets());
+        UpdateScore(0);
+        score = 0;
     }
 
     // Update is called once per frame
@@ -28,4 +34,11 @@ public class GameManager : MonoBehaviour
         }
         
     }
+
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
+    }
 }
+
